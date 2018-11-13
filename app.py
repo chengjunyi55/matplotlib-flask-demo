a62e197248e395ed3fc5aac32fe427b8c5fc9458
chengjunyi55 <43806259+chengjunyi55@users.noreply.github.com>
2018-11-12 21:44:29 -0800

    Update app.py

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ def create_figure():
     return fig
 
 if __name__=="__main__":
-    port = port = int(os.environ.get('PORT', 54321))
+    port = port = int(os.environ.get('PORT', 6000))
     app.run(debug=True, port=port)
